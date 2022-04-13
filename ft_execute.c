9b0c9444c8ae4e85f6158c7e07ba2b3f7ec75580
Marouane  Benbajja <mbenbajj@e2r8p2.1337.ma>
2022-04-13 22:40:19 +0000

    execution 30% : DONE

--- ft_execute.c
+++ ft_execute.c
@@ -26,25 +26,20 @@ void	execute_cmd(t_cmd_pack *cmd_pack, int ac, char **av, char **env)
 	int		id;
 
 	if (!ft_strncmp(av[1], "here_doc", 8))
-		idx = 2;
+		idx = 0;
 	else
-		idx = 1;
+		idx = -1;
 	fd = (int *)malloc(sizeof(int) * 2);
-	while (++idx < ac - 1)
+	while (++idx < ac - 3)
 	{
 		ft_pip(&fd);
 		id = ft_fork();
 		if (id == 0)
 		{
-			printf("cmd : %s	-	path :	%s %d-%d\n", cmd_pack[idx].cmd[0], cmd_pack[idx].cmd_path, cmd_pack[idx].in_fd, cmd_pack[idx].out_fd);
-			// printf("i am in the child - %d -\n", idx);
-			// // dup2(cmd_pack->out_fd, 1);
-			// execve(cmd_pack[idx].cmd_path, cmd_pack[idx].cmd, env);
-			// printf("after execve\n");
-			//exit (0);
+			 execve(cmd_pack[idx].cmd_path, cmd_pack[idx].cmd, env);
 		}
 	}
 	free(fd);
-	// system("leaks pipex");
+	system("leaks pipex");
 	waitpid(id, NULL, 0);
 }
